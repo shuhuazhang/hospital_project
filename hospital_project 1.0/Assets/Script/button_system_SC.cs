ad5fa2e783de08f0b29a2539cc8d667ddfba60f0
nickOhYoung <ohyoung312@gmail.com>
2018-04-04 01:49:14 +0800

    1.2
    音乐开关   部分暂缺
    start场景基本完成

---
+++ hospital_project 1.0/Assets/Script/button_system_SC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class button_system_SC : MonoBehaviour {
+
+    public GameObject START_B, DOCTOR_B, HISTORY_B, LEFT_B, MUSIC_B;
+
+    public void START_B_click()
+    {
+        Debug.Log("START_B_click succed");
+        SceneManager.LoadScene("SECOND");
+    }
+    public void DOCTOR_B_click()
+    {
+        Debug.Log("DOCTOR_B_click succed");
+        SceneManager.LoadScene("DOCTOR");
+    }
+    public void HISTORY_B_click()
+    {
+        Debug.Log("HISTORY_B_click succed");
+        SceneManager.LoadScene("HISTORY");
+    }
+    public void LEFT_B_click()
+    {
+        Debug.Log("LEFT_B_click succed");
+        Application.Quit();
+    }
+    public void MUSIC_B_click()
+    {
+        Debug.Log("MUSIC_B_click succed");
+        //音乐模块暂时未完成
+    }
+}
--- hospital_project 1.0/Assets/Script/logo_change_SC.cs
+++ hospital_project 1.0/Assets/Script/logo_change_SC.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class logo_change_SC : MonoBehaviour {
     public Vector3 rotoation_speed=new Vector3(0f,1f,0f) ;
-	// Use this for initialization
+	// 暂时未启用
 	void Start () {
         GameObject logo = this.gameObject;  
 	}
 
-    // Update is called once per frame
+   
     private void FixedUpdate()
     {
         this.transform.eulerAngles += rotoation_speed;
