8a8ffd65638428e0337cd33aa17e2136edea540b
nickOhYoung <ohyoung312@gmail.com>
2018-04-04 00:43:25 +0800

    1.1
    开始界面完成
    ps:按钮脚本未完成

---
+++ hospital_project 1.0/Assets/Script/logo_change_SC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logo_change_SC : MonoBehaviour {
+    public Vector3 rotoation_speed=new Vector3(0f,1f,0f) ;
+	// Use this for initialization
+	void Start () {
+        GameObject logo = this.gameObject;  
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        this.transform.eulerAngles += rotoation_speed;
+    }
+    
+}
