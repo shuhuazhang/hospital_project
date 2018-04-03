87b5916df83f7f36de91839863c009358b8d9f26
nickOhYoung <ohyoung312@gmail.com>
2018-04-04 03:05:39 +0800

    1.3
    基本完成缺少UI拖拽交互

---
+++ hospital_project 1.0/Assets/Script/UI_event_System_SC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_event_System_SC : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            if(EventSystem.current.IsPointerOverGameObject())
+
+				Debug.Log("当前触摸在UI上");
+			
+			else 
+				Debug.Log("当前没有触摸在UI上");
+        }//此处需要在Android上跑时需要进行修改 谨记
+    }
+}
